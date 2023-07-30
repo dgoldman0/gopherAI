bd5dd4d7f4a346493dd15cf21d28bcfb12e6e67d
Daniel <contact@danielgoldman.us>
2023-07-29 21:02:44 -0400

    Cleanup

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-path = "/home/kir/Documents/Obsidian Vault/posts/2019-05-21_The-world-isn-t-for-anyone--but-we-are-part-of-this-world--and-yeah--fishing-is-fine--921e549b28ce.html"
-
-print(os.path.getsize(path))
